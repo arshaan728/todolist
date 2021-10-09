046cd1641df198ae98f31e38c31cb20fef884eb9
unknown <you@example.com>
2021-10-09 22:28:14 +0530

    my first commit

--- todo/main.js
+++ todo/main.js
@@ -1,3 +1,4 @@
+/*java script part*/
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
